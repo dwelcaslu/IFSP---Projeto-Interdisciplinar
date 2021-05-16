37c4c11dc36c7caccecb88da999cdd87bd71d726
Weld Lucas Cunhas <cunha.lucas012@gmail.com>
2021-05-16 11:37:30 -0300

    Criação de estrutura do projeto de ciência de dados

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projeto Interdisciplinar IFSP - Especialização em Ciência de dados 2021',
+    author='Your name (or your organization/company/team)',
+    license='MIT',
+)
